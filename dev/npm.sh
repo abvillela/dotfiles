ae50487ee055dd102dfd56ac6becdb25f725b5f4
Andrea Villela <andrea@villela.co>
2020-12-13 17:22:18 -0500

    Removed unnecessary npm dependencies.

--- dev/npm.sh
+++ dev/npm.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
-npm i -g @babel/core \
-  @babel/cli \
-  eslint \
-  gatsby-cli \
-  glyphhanger \
-  graphqurl \
-  graphql-tools \
-  ngrok \
-  npm-fzf \
-  prettier \
-  simple-node-logger \
-  webpack-cli
+npm i -g glyphhanger
