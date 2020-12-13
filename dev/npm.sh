3e8e964c612b1b55937814f7496a06c537dc38f2
Andrea Villela <andrea@villela.co>
2020-12-13 17:09:23 -0500

    Added development install scripts.

---
+++ dev/npm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+npm i -g @babel/core \
+  @babel/cli \
+  eslint \
+  gatsby-cli \
+  glyphhanger \
+  graphqurl \
+  graphql-tools \
+  ngrok \
+  npm-fzf \
+  prettier \
+  simple-node-logger \
+  webpack-cli
---
+++ dev/pip.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+python3 -m pip install autopep8 \
+  b2 \
+  brotli \
+  flake8 \
+  fonttools \
+  gitpython \
+  ipython \
+  Pygments \
+  python-dotenv \
+  proselint \
+  requests \
+  vim-vint \
+  virtualenv \
+  virtualenvwrapper
